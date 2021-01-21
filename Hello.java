4ec45c528aa830437b8b55b1e0d9d3d91f505aa6
deyaninta <61777813+deyaninta@users.noreply.github.com>
2021-01-21 22:35:32 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 class Hello{
 	public static void main(String[] args){
+		System.out.println("Judul");
 		System.out.println("Deyaninta: Aplikasi Java 2021");
+		System.out.println("--- *** ---");
 		System.out.println("END");
-		System.out.println("User2")
-		System.out.println("User2")
+		System.out.println("User 2");
 	}
 }
